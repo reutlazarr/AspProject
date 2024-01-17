f0e50393bbc3cd29d5a6149e7080d1f60e30e522
Reut Lazar <reootlazar@gmail.com>
2024-01-17 17:36:50 +0200

    add_bloom_filter_test

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
+#include "../tests/getters_bloom_filter_test.cpp"
+
 int main(int argc, char** argv) {
-::testing::InitGoogleTest(&argc, argv);
-return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
