666ce1d4681ef1494ace555371515b7e40771045
Reut Lazar <reootlazar@gmail.com>
2024-01-24 16:07:23 +0200

    updated bloomFilter header

--- src/bloomFilter.cpp
+++ src/bloomFilter.cpp
@@ -4,22 +4,17 @@
 #include "../headers/HashFunction1.h"
 #include "../headers/HashFunction2.h"
 #include "../headers/BloomFilter.h"
-#include <iostream>
+
 
 // constractor
-BloomFilter::BloomFilter(int sizeArray, int numHashFunctions, std::vector<std::unique_ptr<IHashFunction>> hashFunctions) 
+BloomFilter::BloomFilter(int sizeArray, std::vector<std::unique_ptr<IHashFunction>> hashFunctions) 
 : bitArray(sizeArray, false), hashFunctions(std::move(hashFunctions)) {
     this->sizeArray = sizeArray;
-    this->numHashFunctions = numHashFunctions;
 }
 
 //getters
 int BloomFilter::getSizeArray() {
     return sizeArray;
-}
-
-int BloomFilter::getNumHashFunctions() {
-    return numHashFunctions;
 }
 
 const std::vector<std::unique_ptr<IHashFunction>>& BloomFilter::getHashFunctions() const {
