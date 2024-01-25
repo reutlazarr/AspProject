fcd115f4e5373a2e3f9e9549144eb3b35646c89d
BarVinizky <barr98@gmail.com>
2024-01-25 18:03:04 +0200

    add CheckInput.h

---
+++ headers/CheckInput.h
@@ -0,0 +1,24 @@
+// CheckInput.h
+
+#include "IHashFunction.h"
+#include <map>
+#include <vector>
+#include <set>
+#include <memory>
+#include <sstream>
+#include<string>
+
+
+class CheckInput {
+private:
+    std::map<int, std::unique_ptr<IHashFunction>> hashFunctionsMap;
+
+public:
+    // constractor
+    CheckInput(std::map<int, std::unique_ptr<IHashFunction>> hashFunctionsMap);
+    //std::stringstream nextCommand(); // return the next command fron the user
+    bool checkExecuteCommand(std::stringstream& input); // check the user command
+    bool checkArraySize(std::istream& input); // check the user input of arraySize
+    bool checkHashFunctions(std::istream& input); // check the user input of hashFunctions
+};
+
