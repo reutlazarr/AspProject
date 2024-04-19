3500c8e63d81c0577fc76c98b4db0a84b3f7d778
BarVinizky <155451112+BarVinizky@users.noreply.github.com>
2024-04-19 12:04:47 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/Reut/Documents/Reut/degree/C/Asp/Project/AspProject
-# Build directory: /mnt/c/Users/Reut/Documents/Reut/degree/C/Asp/Project/AspProject/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(CalcTests "/mnt/c/Users/Reut/Documents/Reut/degree/C/Asp/Project/AspProject/build/CalcTests")
-set_tests_properties(CalcTests PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/Reut/Documents/Reut/degree/C/Asp/Project/AspProject/CMakeLists.txt;52;add_test;/mnt/c/Users/Reut/Documents/Reut/degree/C/Asp/Project/AspProject/CMakeLists.txt;0;")
