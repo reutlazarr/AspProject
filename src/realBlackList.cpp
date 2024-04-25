52320fc507469fd1a07448c8522718ce68e8c438
Reut Lazar <reootlazar@gmail.com>
2024-04-25 12:38:50 +0300

    fixed socket server

--- src/realBlackList.cpp
+++ src/realBlackList.cpp
@@ -1,20 +1,19 @@
-    // realBlackList.cpp
-
 #include <string>
 #include <vector>
 #include "RealBlackList.h"
-#include <algorithm> 
-
+#include <algorithm>
 
 // getters
-std::vector<std::string>& RealBlackList::getRealList() {
+std::vector<std::string> &RealBlackList::getRealList()
+{
     return realBlackList;
 }
 
-bool RealBlackList::isUrlInBlackList(const std::string& url) const {
+bool RealBlackList::isUrlInBlackList(const std::string &url) const
+{
     // Use std::find to check if the URL is in the real blacklist
     auto check = std::find(realBlackList.begin(), realBlackList.end(), url);
-    
+
     // Return true if the URL is found, false otherwise
     return check != realBlackList.end();
-}  
+}
